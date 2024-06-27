897399148e4cfb00f36eaf6aa31c730f40434cb7
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-06-27 11:07:56 +0100

    adds completed checkins to completed tab and updates priority card

--- src/Components/PriorityCheckInCard.jsx
+++ src/Components/PriorityCheckInCard.jsx
@@ -8,7 +8,7 @@ const PriorityCheckInCard = ({
 }) => {
   return (
     <section className="priority-checkin">
-      <h1>{student["current-task"]}</h1>
+      <h1>Current Task: {student["current-task"]}</h1>
       <h2>{student.name}</h2>
       <h4>{student["last-seen"]}</h4>
       {student.partner && <h3>Partner: {student.partner}</h3>}
